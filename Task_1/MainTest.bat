b3b11fee22ec7db304625d37ba5872fc589e8ab2
Mikhail <mikhail.barashkou@yandex.ru>
2023-05-15 23:45:06 +0300

    Update

--- Task_1/MainTest.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_111
-set CLASSPATH=d:\Java_Learn\OOP_lab_1\Task_1\;d:\Java_Learn\OOP_lab_1\Task_1\;lib\junit-jupiter-engine-5.8.1.jar;lib\junit-jupiter-api-5.8.1.jar;lib\apiguardian-api-1.1.0.jar
-java org.junit.platform.console.ConsoleLauncher --scan-classpath
-pause
--- Task_1/MainTest.java
+++ Task_1/MainTest.java
@@ -1,8 +1,8 @@
 package Task_1;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 public class MainTest {
     @Test
